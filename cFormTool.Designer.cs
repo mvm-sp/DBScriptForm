983786350ff650da55c7e51d7f6e71269decb5ea
Marcelo V. Marcondes <43617650+mvm-sp@users.noreply.github.com>
2020-01-28 15:13:24 -0300

    root files

---
+++ cFormTool.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace NESTDataBaseTool
+{
+	partial class cFormTool
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cPanelHost = new System.Windows.Forms.Panel();
+			this.cLablePortOrigem = new System.Windows.Forms.Label();
+			this.cLableHostOrigem = new System.Windows.Forms.Label();
+			this.cLableUserOrigem = new System.Windows.Forms.Label();
+			this.cLabelSenhaOrigem = new System.Windows.Forms.Label();
+			this.cLabelDBOrigem = new System.Windows.Forms.Label();
+			this.cLabelDBDestino = new System.Windows.Forms.Label();
+			this.cTextHostOrigem = new System.Windows.Forms.TextBox();
+			this.cTextPortaOrigem = new System.Windows.Forms.TextBox();
+			this.cTextUsuarioOrigem = new System.Windows.Forms.TextBox();
+			this.cTextSenhaOrigem = new System.Windows.Forms.TextBox();
+			this.cTextDBOrigem = new System.Windows.Forms.TextBox();
+			this.cTextDBDestino = new System.Windows.Forms.TextBox();
+			this.cLabelOrigem = new System.Windows.Forms.Label();
+			this.cLableDestino = new System.Windows.Forms.Label();
+			this.cPanelDestino = new System.Windows.Forms.Panel();
+			this.cTextSenhaDestino = new System.Windows.Forms.TextBox();
+			this.cTextUsuarioDestino = new System.Windows.Forms.TextBox();
+			this.cTextPortaDestino = new System.Windows.Forms.TextBox();
+			this.cTextHostDestino = new System.Windows.Forms.TextBox();
+			this.cLabelSenhaDestino = new System.Windows.Forms.Label();
+			this.cLabelUserDestino = new System.Windows.Forms.Label();
+			this.cLablePortDestino = new System.Windows.Forms.Label();
+			this.cLableHostDestino = new System.Windows.Forms.Label();
+			this.cLableDestino = new System.Windows.Forms.Label();
+			this.cTextDir = new System.Windows.Forms.TextBox();
+			this.cLableDir = new System.Windows.Forms.Label();
+			this.cButtonGerarScripts = new System.Windows.Forms.Button();
+			this.cBottonMigra = new System.Windows.Forms.Button();
+			this.cPanelHost.SuspendLayout();
+			this.cPanelDestino.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// cPanelHost
+			// 
+			this.cPanelHost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cPanelHost.Controls.Add(this.cTextSenhaOrigem);
+			this.cPanelHost.Controls.Add(this.cTextUsuarioOrigem);
+			this.cPanelHost.Controls.Add(this.cTextPortaOrigem);
+			this.cPanelHost.Controls.Add(this.cTextHostOrigem);
+			this.cPanelHost.Controls.Add(this.cLabelSenhaOrigem);
+			this.cPanelHost.Controls.Add(this.cLableUserOrigem);
+			this.cPanelHost.Controls.Add(this.cLablePortOrigem);
+			this.cPanelHost.Controls.Add(this.cLableHostOrigem);
+			this.cPanelHost.Controls.Add(this.cLabelDBOrigem);
+			this.cPanelHost.Controls.Add(this.cTextDBOrigem);
+			this.cPanelHost.Controls.Add(this.cLableHostOrigem);
+			this.cPanelHost.Location = new System.Drawing.Point(12, 29);
+			this.cPanelHost.Name = "cPanelHost";
+			this.cPanelHost.Size = new System.Drawing.Size(343, 170);
+			this.cPanelHost.TabIndex = 1;
+			// 
+			// cLablePortOrigem
+			// 
+			this.cLablePortOrigem.AutoSize = true;
+			this.cLablePortOrigem.Location = new System.Drawing.Point(9, 51);
+			this.cLablePortOrigem.Name = "cLablePortOrigem";
+			this.cLablePortOrigem.Size = new System.Drawing.Size(32, 13);
+			this.cLablePortOrigem.TabIndex = 2;
+			this.cLablePortOrigem.Text = "Porta";
+			// 
+			// cLableHostOrigem
+			// 
+			this.cLableHostOrigem.AutoSize = true;
+			this.cLableHostOrigem.Location = new System.Drawing.Point(9, 19);
+			this.cLableHostOrigem.Name = "cLableHostOrigem";
+			this.cLableHostOrigem.Size = new System.Drawing.Size(29, 13);
+			this.cLableHostOrigem.TabIndex = 1;
+			this.cLableHostOrigem.Text = "Host";
+			// 
+			// cLableUserOrigem
+			// 
+			this.cLableUserOrigem.AutoSize = true;
+			this.cLableUserOrigem.Location = new System.Drawing.Point(9, 78);
+			this.cLableUserOrigem.Name = "cLableUserOrigem";
+			this.cLableUserOrigem.Size = new System.Drawing.Size(43, 13);
+			this.cLableUserOrigem.TabIndex = 3;
+			this.cLableUserOrigem.Text = "Usuario";
+			// 
+			// cLabelSenhaOrigem
+			// 
+			this.cLabelSenhaOrigem.AutoSize = true;
+			this.cLabelSenhaOrigem.Location = new System.Drawing.Point(9, 110);
+			this.cLabelSenhaOrigem.Name = "cLabelSenhaOrigem";
+			this.cLabelSenhaOrigem.Size = new System.Drawing.Size(38, 13);
+			this.cLabelSenhaOrigem.TabIndex = 4;
+			this.cLabelSenhaOrigem.Text = "Senha";
+			// 
+			// cLabelDBOrigem
+			// 
+			this.cLabelDBOrigem.AutoSize = true;
+			this.cLabelDBOrigem.Location = new System.Drawing.Point(9, 140);
+			this.cLabelDBOrigem.Name = "cLabelDBOrigem";
+			this.cLabelDBOrigem.Size = new System.Drawing.Size(38, 13);
+			this.cLabelDBOrigem.TabIndex = 4;
+			this.cLabelDBOrigem.Text = "DB";
+			// 
+			// cTextHostOrigem
+			// 
+			this.cTextHostOrigem.Location = new System.Drawing.Point(51, 15);
+			this.cTextHostOrigem.Name = "cTextHostOrigem";
+			this.cTextHostOrigem.Size = new System.Drawing.Size(276, 20);
+			this.cTextHostOrigem.TabIndex = 5;
+			// 
+			// cTextPortaOrigem
+			// 
+			this.cTextPortaOrigem.Location = new System.Drawing.Point(51, 45);
+			this.cTextPortaOrigem.Name = "cTextPortaOrigem";
+			this.cTextPortaOrigem.Size = new System.Drawing.Size(276, 20);
+			this.cTextPortaOrigem.TabIndex = 6;
+			// 
+			// cTextUsuarioOrigem
+			// 
+			this.cTextUsuarioOrigem.Location = new System.Drawing.Point(51, 75);
+			this.cTextUsuarioOrigem.Name = "cTextUsuarioOrigem";
+			this.cTextUsuarioOrigem.Size = new System.Drawing.Size(276, 20);
+			this.cTextUsuarioOrigem.TabIndex = 7;
+			// 
+			// cTextSenhaOrigem
+			// 
+			this.cTextSenhaOrigem.Location = new System.Drawing.Point(51, 105);
+			this.cTextSenhaOrigem.Name = "cTextSenhaOrigem";
+			this.cTextSenhaOrigem.PasswordChar = '*';
+			this.cTextSenhaOrigem.Size = new System.Drawing.Size(276, 20);
+			this.cTextSenhaOrigem.TabIndex = 8;
+			// 
+			// cTextDBOrigem
+			// 
+			this.cTextDBOrigem.Location = new System.Drawing.Point(51, 135);
+			this.cTextDBOrigem.Name = "cTextDBOrigem";
+			this.cTextDBOrigem.Size = new System.Drawing.Size(276, 20);
+			this.cTextDBOrigem.TabIndex = 8;
+			// 
+			// cLabelOrigem
+			// 
+			this.cLabelOrigem.AutoSize = true;
+			this.cLabelOrigem.Location = new System.Drawing.Point(20, 13);
+			this.cLabelOrigem.Name = "cLabelOrigem";
+			this.cLabelOrigem.Size = new System.Drawing.Size(40, 13);
+			this.cLabelOrigem.TabIndex = 2;
+			this.cLabelOrigem.Text = "Origem";
+			// 
+			// cLableDestino
+			// 
+			this.cLableDestino.AutoSize = true;
+			this.cLableDestino.Location = new System.Drawing.Point(389, 13);
+			this.cLableDestino.Name = "cLableDestino";
+			this.cLableDestino.Size = new System.Drawing.Size(43, 13);
+			this.cLableDestino.TabIndex = 4;
+			this.cLableDestino.Text = "Destino";
+			// 
+			// cPanelDestino
+			// 
+			this.cPanelDestino.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.cPanelDestino.Controls.Add(this.cTextSenhaDestino);
+			this.cPanelDestino.Controls.Add(this.cTextUsuarioDestino);
+			this.cPanelDestino.Controls.Add(this.cTextPortaDestino);
+			this.cPanelDestino.Controls.Add(this.cTextHostDestino);
+			this.cPanelDestino.Controls.Add(this.cLabelSenhaDestino);
+			this.cPanelDestino.Controls.Add(this.cLabelUserDestino);
+			this.cPanelDestino.Controls.Add(this.cLablePortDestino);
+			this.cPanelDestino.Controls.Add(this.cLableHostDestino);
+			this.cPanelDestino.Controls.Add(this.cLabelDBDestino);
+			this.cPanelDestino.Controls.Add(this.cTextDBDestino);
+			this.cPanelDestino.Location = new System.Drawing.Point(381, 29);
+			this.cPanelDestino.Name = "cPanelDestino";
+			this.cPanelDestino.Size = new System.Drawing.Size(343, 170);
+			this.cPanelDestino.TabIndex = 3;
+			// 
+			// cTextSenhaDestino
+			// 
+			this.cTextSenhaDestino.Location = new System.Drawing.Point(51, 105);
+			this.cTextSenhaDestino.Name = "cTextSenhaDestino";
+			this.cTextSenhaDestino.PasswordChar = '*';
+			this.cTextSenhaDestino.Size = new System.Drawing.Size(276, 20);
+			this.cTextSenhaDestino.TabIndex = 8;
+			// 
+			// cTextDBDestino
+			// 
+			this.cTextDBDestino.Location = new System.Drawing.Point(51, 135);
+			this.cTextDBDestino.Name = "cTextDBDestino";
+			this.cTextDBDestino.Size = new System.Drawing.Size(276, 20);
+			this.cTextDBDestino.TabIndex = 8;
+			// 
+			// cTextUsuarioDestino
+			// 
+			this.cTextUsuarioDestino.Location = new System.Drawing.Point(51, 75);
+			this.cTextUsuarioDestino.Name = "cTextUsuarioDestino";
+			this.cTextUsuarioDestino.Size = new System.Drawing.Size(276, 20);
+			this.cTextUsuarioDestino.TabIndex = 7;
+			// 
+			// cTextPortaDestino
+			// 
+			this.cTextPortaDestino.Location = new System.Drawing.Point(51, 45);
+			this.cTextPortaDestino.Name = "cTextPortaDestino";
+			this.cTextPortaDestino.Size = new System.Drawing.Size(276, 20);
+			this.cTextPortaDestino.TabIndex = 6;
+			// 
+			// cTextHostDestino
+			// 
+			this.cTextHostDestino.Location = new System.Drawing.Point(51, 15);
+			this.cTextHostDestino.Name = "cTextHostDestino";
+			this.cTextHostDestino.Size = new System.Drawing.Size(276, 20);
+			this.cTextHostDestino.TabIndex = 5;
+			// 
+			// cLabelSenhaDestino
+			// 
+			this.cLabelSenhaDestino.AutoSize = true;
+			this.cLabelSenhaDestino.Location = new System.Drawing.Point(9, 110);
+			this.cLabelSenhaDestino.Name = "cLabelSenhaDestino";
+			this.cLabelSenhaDestino.Size = new System.Drawing.Size(38, 13);
+			this.cLabelSenhaDestino.TabIndex = 4;
+			this.cLabelSenhaDestino.Text = "Senha";
+			// 
+			// cLabelDBDestino
+			// 
+			this.cLabelDBDestino.AutoSize = true;
+			this.cLabelDBDestino.Location = new System.Drawing.Point(9, 140);
+			this.cLabelDBDestino.Name = "cLabelDBDestino";
+			this.cLabelDBDestino.Size = new System.Drawing.Size(38, 13);
+			this.cLabelDBDestino.TabIndex = 4;
+			this.cLabelDBDestino.Text = "DB";
+
+			// 
+			// cLabelUserDestino
+			// 
+			this.cLabelUserDestino.AutoSize = true;
+			this.cLabelUserDestino.Location = new System.Drawing.Point(9, 78);
+			this.cLabelUserDestino.Name = "cLabelUserDestino";
+			this.cLabelUserDestino.Size = new System.Drawing.Size(43, 13);
+			this.cLabelUserDestino.TabIndex = 3;
+			this.cLabelUserDestino.Text = "Usuario";
+			// 
+			// cLablePortDestino
+			// 
+			this.cLablePortDestino.AutoSize = true;
+			this.cLablePortDestino.Location = new System.Drawing.Point(9, 51);
+			this.cLablePortDestino.Name = "cLablePortDestino";
+			this.cLablePortDestino.Size = new System.Drawing.Size(32, 13);
+			this.cLablePortDestino.TabIndex = 2;
+			this.cLablePortDestino.Text = "Porta";
+			// 
+			// cLableHostDestino
+			// 
+			this.cLableHostDestino.AutoSize = true;
+			this.cLableHostDestino.Location = new System.Drawing.Point(9, 19);
+			this.cLableHostDestino.Name = "cLableHostDestino";
+			this.cLableHostDestino.Size = new System.Drawing.Size(29, 13);
+			this.cLableHostDestino.TabIndex = 1;
+			this.cLableHostDestino.Text = "Host";
+			// 
+			// cTextDir
+			// 
+			this.cTextDir.Location = new System.Drawing.Point(62, 221);
+			this.cTextDir.Name = "cTextDir";
+			this.cTextDir.Size = new System.Drawing.Size(438, 20);
+			this.cTextDir.TabIndex = 7;
+			// 
+			// cLableDir
+			// 
+			this.cLableDir.AutoSize = true;
+			this.cLableDir.Location = new System.Drawing.Point(14, 225);
+			this.cLableDir.Name = "cLableDir";
+			this.cLableDir.Size = new System.Drawing.Size(46, 13);
+			this.cLableDir.TabIndex = 6;
+			this.cLableDir.Text = "Diretório";
+			// 
+			// cButtonGerarScripts
+			// 
+			this.cButtonGerarScripts.Location = new System.Drawing.Point(506, 221);
+			this.cButtonGerarScripts.Name = "cButtonGerarScripts";
+			this.cButtonGerarScripts.Size = new System.Drawing.Size(105, 23);
+			this.cButtonGerarScripts.TabIndex = 8;
+			this.cButtonGerarScripts.Text = "Scripts";
+			this.cButtonGerarScripts.UseVisualStyleBackColor = true;
+			this.cButtonGerarScripts.Click += new System.EventHandler(this.cButtonGerarScripts_Click);
+			// 
+			// cBottonMigra
+			// 
+			this.cBottonMigra.Location = new System.Drawing.Point(627, 221);
+			this.cBottonMigra.Name = "cBottonMigra";
+			this.cBottonMigra.Size = new System.Drawing.Size(105, 23);
+			this.cBottonMigra.TabIndex = 9;
+			this.cBottonMigra.Text = "Migrar Dados";
+			this.cBottonMigra.UseVisualStyleBackColor = true;
+			this.cBottonMigra.Click += new System.EventHandler(this.cButtonMigra_Click);
+			// 
+			// cFormTool
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(754, 253);
+			this.Controls.Add(this.cBottonMigra);
+			this.Controls.Add(this.cButtonGerarScripts);
+			this.Controls.Add(this.cTextDir);
+			this.Controls.Add(this.cLableDir);
+			this.Controls.Add(this.cLableDestino);
+			this.Controls.Add(this.cPanelDestino);
+			this.Controls.Add(this.cLabelOrigem);
+			this.Controls.Add(this.cPanelHost);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "cFormTool";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "NEST Database Tool";
+			this.Load += new System.EventHandler(this.cFormTool_Load);
+			this.cPanelHost.ResumeLayout(false);
+			this.cPanelHost.PerformLayout();
+			this.cPanelDestino.ResumeLayout(false);
+			this.cPanelDestino.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel cPanelHost;
+		private System.Windows.Forms.Label cLablePortOrigem;
+		private System.Windows.Forms.Label cLableHostOrigem;
+		private System.Windows.Forms.TextBox cTextSenhaOrigem;
+		private System.Windows.Forms.TextBox cTextUsuarioOrigem;
+		private System.Windows.Forms.TextBox cTextPortaOrigem;
+		private System.Windows.Forms.TextBox cTextHostOrigem;
+		private System.Windows.Forms.Label cLabelSenhaOrigem;
+		private System.Windows.Forms.Label cLabelDBOrigem;
+		private System.Windows.Forms.Label cLabelDBDestino;
+		private System.Windows.Forms.Label cLableUserOrigem;
+		private System.Windows.Forms.Label cLabelOrigem;
+		private System.Windows.Forms.Label cLableDestino;
+		private System.Windows.Forms.Panel cPanelDestino;
+		private System.Windows.Forms.TextBox cTextSenhaDestino;
+		private System.Windows.Forms.TextBox cTextUsuarioDestino;
+		private System.Windows.Forms.TextBox cTextPortaDestino;
+		private System.Windows.Forms.TextBox cTextHostDestino;
+		private System.Windows.Forms.TextBox cTextDBDestino;
+		private System.Windows.Forms.TextBox cTextDBOrigem;
+		private System.Windows.Forms.Label cLabelSenhaDestino;
+		private System.Windows.Forms.Label cLabelUserDestino;
+		private System.Windows.Forms.Label cLablePortDestino;
+		private System.Windows.Forms.Label cLableHostDestino;
+		private System.Windows.Forms.TextBox cTextDir;
+		private System.Windows.Forms.Label cLableDir;
+		private System.Windows.Forms.Button cButtonGerarScripts;
+		private System.Windows.Forms.Button cBottonMigra;
+	}
+}
+
---
+++ cFormTool.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NESTExportaDB;
+
+namespace NESTDataBaseTool
+{
+	public partial class cFormTool : Form
+	{
+		public cFormTool()
+		{
+			InitializeComponent();
+		}
+
+		public void cFormTool_Load(object sender, EventArgs e)
+		{
+			LerPropriedades();
+		}
+
+		public void cButtonGerarScripts_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				this.Cursor = Cursors.WaitCursor;
+				cButtonGerarScripts.Enabled = false;
+				cBottonMigra.Enabled = false;
+				ClienteBanco client;
+				SalvaPropriedades();
+				string mServe = cTextHostOrigem.Text;
+				int mPort = int.Parse(cTextPortaOrigem.Text);
+				string mUser = cTextUsuarioOrigem.Text;
+				string mPass = cTextSenhaOrigem.Text;
+				string mDBName = cTextDBOrigem.Text;
+				string mDir = cTextDir.Text;
+
+				CriaDiretorio(mDir + "Dados\\");
+
+				//client = new ClienteBanco(DbTypes.PgSql, "201.20.7.33", 5890, "postgres", "TjYz3m", null, "DES_NESTRIS");
+				client = new ClienteBanco(DbTypes.PgSql, mServe, mPort, mUser, mPass, null, mDBName);
+
+				client.ScriptBanco(@mDir);
+				client.ScriptProcedures(@mDir);
+				client.ScriptDados(@mDir + "Dados\\");
+
+				mServe = cTextHostDestino.Text;
+				mPort = int.Parse(cTextPortaDestino.Text);
+				mUser = cTextUsuarioDestino.Text;
+				mPass = cTextSenhaDestino.Text;
+				mDBName = cTextDBDestino.Text;
+
+				//client = new ClienteBanco(DbTypes.PgSql, "201.20.7.33", 5890, "postgres", "TjYz3m", null, "DES_NESTRIS");
+				client = new ClienteBanco(DbTypes.PgSql, mServe, mPort, mUser, mPass, null, mDBName);
+				client.ScriptBanco(@mDir);
+				client.ScriptProcedures(@mDir);
+				cButtonGerarScripts.Enabled = true;
+				cBottonMigra.Enabled = true;
+				this.Cursor = Cursors.Default;
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Erro ao Construir Scripts: " + Environment.NewLine + ex.ToString());
+				cButtonGerarScripts.Enabled = true;
+				cBottonMigra.Enabled = true;
+				this.Cursor = Cursors.Default;
+			}
+		}
+
+		public void cButtonMigra_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				this.Cursor = Cursors.WaitCursor;
+				cButtonGerarScripts.Enabled = false;
+				cBottonMigra.Enabled = false;
+				ClienteBanco client;
+				SalvaPropriedades();
+
+				string mServe = cTextHostDestino.Text;
+				int mPort = int.Parse(cTextPortaDestino.Text);
+				string mUser = cTextUsuarioDestino.Text;
+				string mPass = cTextSenhaDestino.Text;
+				string mDBName = cTextDBDestino.Text;
+				string mDir = cTextDir.Text;
+
+				CriaDiretorio(mDir + "Dados\\");
+
+				//client = new ClienteBanco(DbTypes.PgSql, "201.20.7.33", 5890, "postgres", "TjYz3m", null, "DES_NESTRIS");
+				client = new ClienteBanco(DbTypes.PgSql, mServe, mPort, mUser, mPass, null, mDBName);
+				client.ImportaDados(@mDir  + "Dados\\");
+
+				cButtonGerarScripts.Enabled = true;
+				cBottonMigra.Enabled = true;
+				this.Cursor = Cursors.Default;
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Erro ao Importar Dados " + Environment.NewLine + ex.ToString());
+				cButtonGerarScripts.Enabled = true;
+				cBottonMigra.Enabled = true;
+				this.Cursor = Cursors.Default;
+			}
+		}
+		private void SalvaPropriedades()
+		{
+			Settings.Default.HostOrigem = cTextHostOrigem.Text;
+			Settings.Default.HostDestino = cTextHostDestino.Text;
+			Settings.Default.UsuarioOrigem = cTextUsuarioOrigem.Text;
+			Settings.Default.UsuarioDestino = cTextUsuarioDestino.Text;
+			Settings.Default.SenhaOrigem = cTextSenhaOrigem.Text;
+			Settings.Default.SenhaDestino = cTextSenhaDestino.Text;
+			Settings.Default.PortaOrigem = cTextPortaOrigem.Text;
+			Settings.Default.PortaDestino = cTextPortaDestino.Text;
+			Settings.Default.DBOrigem = cTextDBOrigem.Text;
+			Settings.Default.DBDestino = cTextDBDestino.Text;
+			Settings.Default.Diretorio = cTextDir.Text;
+
+			Settings.Default.Save();
+
+		}
+
+		private void LerPropriedades()
+		{
+
+			cTextHostOrigem.Text = Settings.Default.HostOrigem;
+			cTextHostDestino.Text = Settings.Default.HostDestino;
+			cTextUsuarioOrigem.Text = Settings.Default.UsuarioOrigem;
+			cTextUsuarioDestino.Text = Settings.Default.UsuarioDestino;
+			cTextSenhaOrigem.Text = Settings.Default.SenhaOrigem;
+			cTextSenhaDestino.Text = Settings.Default.SenhaDestino;
+			cTextPortaOrigem.Text = Settings.Default.PortaOrigem;
+			cTextPortaDestino.Text = Settings.Default.PortaDestino;
+			cTextDBOrigem.Text = Settings.Default.DBOrigem;
+			cTextDBDestino.Text = Settings.Default.DBDestino;
+			cTextDir.Text = Settings.Default.Diretorio;
+
+
+		}
+
+		private void CriaDiretorio(string path)
+		{
+			bool folderExists = System.IO.Directory.Exists(path);
+			if (!folderExists)
+				System.IO.Directory.CreateDirectory(path);
+		}
+	}
+}
